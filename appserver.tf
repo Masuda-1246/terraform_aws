1267d67e55f0bbe464ffc8bfde30117aef765a13
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-08-07 15:43:31 +0900

    apply ec2

--- appserver.tf
+++ appserver.tf
@@ -9,3 +9,18 @@ resource "aws_key_pair" "keypair" {
     Environment = var.environment
   }
 }
+
+resource "aws_instance" "app_server" {
+  ami                    = data.aws_ami.app.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.keypair.key_name
+  subnet_id              = aws_subnet.public_subnet_1a.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id, aws_security_group.opmg_sg.id]
+
+  tags = {
+    Name        = "${var.project}-${var.environment}-app"
+    Project     = var.project
+    Environment = var.environment
+    Type        = "app"
+  }
+}
